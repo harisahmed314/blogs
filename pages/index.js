bb99ed59d3f2f274f94b012574456c0dcdf70b78
harisahmed314 <harisahmed314@gmail.com>
2023-08-05 11:51:24 +0500

    footer add

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import BloggersList from "@/components/Blogs-frame/BlogsFrame";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/footer/footer";
+
 
 
 
 
 
 export default function Home() {
-  return <><Header /><BloggersList/></>
+  return <><Header /><BloggersList/>
+  <Footer/> </>
 }
