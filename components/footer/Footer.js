d70493c90bb88cb6a196a84f2b2ae92057795547
harisahmed314 <harisahmed314@gmail.com>
2023-08-05 11:52:59 +0500

    change color of footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer=()=>{
     return(
-        <footer class="text-gray-600 body-font">
+        <footer class="text-gray-600 bg-pink-300 body-font">
   <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
